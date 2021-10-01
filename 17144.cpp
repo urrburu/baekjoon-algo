a36ce3115f801a13533c9a6b4ed02020177a7b3e
JeongChanHwi <bbs4614@naver.com>
2021-10-01 20:13:50 +0900

    21.10.01

--- 17144.cpp
+++ 17144.cpp
@@ -209,3 +209,178 @@ int main() {
 	return 0;
 }
 
+#include<iostream>
+#include<vector>
+#include<cstring>
+#include <algorithm>
+using namespace std;
+int r, c, t;
+int Now[55][55];
+int Next[55][55];
+int dx[] = { 1,0,-1,0 };
+int dy[] = { 0,1,0,-1 };
+vector<int> pure;
+bool OOB(int x, int y) { return  x < 0 || y < 0 || x >= r || y >= c; }
+void spread() {
+	memset(Next, 0, sizeof(Next));
+	for (int i = 0; i < r; ++i) {
+		for (int j = 0; j < c; ++j) {
+			if (Now[i][j] != 0) {
+				int sp = Now[i][j] / 5;
+				for (int tmp = 0; tmp < 4; ++tmp) {
+					int nx = i + dx[tmp]; int ny = j + dy[tmp];
+					if (!OOB(nx, ny) && Now[nx][ny] != -1) {
+						Next[nx][ny] += sp;
+						Now[i][j] -= sp;
+					}
+				}
+				Next[i][j] += Now[i][j];
+			}
+		}
+	}
+	for (int i = 0; i < r; ++i) {
+		for (int j = 0; j < c; ++j) {
+			Now[i][j] = Next[i][j];
+		}
+	}
+}
+//void circular() {
+//	memset(Next, 0, sizeof(Next));
+//	int up_x = pure[0];
+//	int up_y = 1;
+//	
+//	do {
+//		Next[up_x][up_y + 1] = Now[up_x][up_y];
+//		up_y++;
+//	} while (up_y < c - 1);
+//	
+//	do {
+//		Next[up_x - 1][up_y] = Now[up_x][up_y];
+//		up_x--;
+//	} while (up_x > 0);
+//	
+//	do {
+//		Next[up_x][up_y - 1] = Now[up_x][up_y];
+//		up_y--;
+//	} while (up_y > 0);
+//	do {
+//		Next[up_x+1][up_y] = Now[up_x][up_y];
+//		up_x++;
+//	} while (up_x < pure[0]);
+//	
+//	int down_x = pure[1];
+//	int down_y = 1;
+//	do {
+//		Next[down_x][down_y + 1] = Now[down_x][down_y];
+//		down_y++;
+//	} while (down_y < c - 1);
+//	do {
+//		Next[down_x + 1][down_y] = Now[down_x][down_y];
+//		down_x++;
+//	} while (down_x < r - 1);
+//	do {
+//		Next[down_x][down_y - 1] = Now[down_x][down_y];
+//		down_y--;
+//	} while (down_y > 0);
+//	do {
+//		Next[down_x - 1][down_y] = Now[down_x][down_y];
+//		down_x--;
+//	} while (down_x < pure[1]);
+//	for (int i = 1; i < pure[0]; ++i) {
+//		for (int j = 1; j < c-1; ++j) {
+//			Next[i][j] = Now[i][j];
+//		}
+//	}
+//	for (int i = pure[1]+1; i < r - 1; ++i) {
+//		for (int j = 1; j < c - 1; ++j) {
+//			Next[i][j] = Now[i][j];
+//		}
+//	}
+//	Next[pure[0]][0] = -1;
+//	Next[pure[1]][0] = -1;
+//	for (int i = 0; i < r; ++i) {
+//		for (int j = 0; j < c; ++j) {
+//			Now[i][j] = Next[i][j];
+//		}
+//	}
+//
+//}
+void circular() {
+	cout << "\n";
+	for (int i = pure[0]; i > 0; --i) {
+		Now[i][0] = Now[i - 1][0];
+	}
+	Now[pure[0]][0] = -1;
+	for (int i = 0; i < c - 1; ++i) {
+		Now[0][i] = Now[0][i + 1];
+	}
+	for (int i = 0; i < pure[0]; ++i) {
+		Now[i][c - 1] = Now[i + 1][c - 1];
+	}
+	for (int i = c - 1; i > 0; --i) {
+		Now[pure[0]][i] = Now[pure[0]][i - 1];
+	}
+
+	for (int i = pure[1]; i < r - 1; ++i) {
+		Now[i][0] = Now[i + 1][0];
+	}
+	Now[pure[1]][0] = -1;
+	for (int i = 0; i < c - 1; ++i) {
+		Now[r - 1][i] = Now[r - 1][i + 1];
+	}
+	for (int i = r - 1; i > pure[1]; --i) {
+		Now[i][c - 1] = Now[i - 1][c - 1];
+	}
+	for (int i = c - 1; i > 0; --i) {
+		Now[pure[1]][i] = Now[pure[1]][i - 1];
+	}
+	for (int i = 0; i < r; ++i) {
+		for (int j = 0; j < c; ++j) {
+			if (Now[i][j] == -1)Now[i][j] = 0;
+		}
+	}
+
+
+}
+int main() {
+	cin >> r >> c >> t;
+	for (int i = 0; i < r; ++i) {
+		for (int j = 0; j < c; ++j) {
+			cin >> Now[i][j];
+			if (Now[i][j] == -1) { pure.push_back(i); }
+		}
+	}
+	int ans = 0;
+	while (t--) {
+		spread();
+		for (int i = 0; i < r; ++i) {
+			for (int j = 0; j < c; ++j) {
+				cout << Now[i][j] << " ";
+			}
+			cout << "\n";
+		}
+		cout << "\n";
+		circular();
+		cout << "\n";
+		for (int i = 0; i < r; ++i) {
+			for (int j = 0; j < c; ++j) {
+				cout << Now[i][j] << " ";
+			}
+			cout << "\n";
+		}
+		cout << "\n";
+		int tmp = 0;
+		for (int i = 0; i < r; ++i) {
+			for (int j = 0; j < c; ++j) {
+				if (Now[i][j] != -1)tmp += Now[i][j];
+			}
+		}
+		cout << tmp << "\n";
+	}
+	for (int i = 0; i < r; ++i) {
+		for (int j = 0; j < c; ++j) {
+			if (Now[i][j] != -1)ans += Now[i][j];
+		}
+	}
+	cout << ans << "\n";
+}
